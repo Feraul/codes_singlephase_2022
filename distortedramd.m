bb1406a0e34073c8577d4fd809c58d64d30b97ab
Fernando Contreras <ferlicapac@gmail.com>
2023-08-07 04:33:40 -0300

    ok

--- distortedramd.m
+++ distortedramd.m
@@ -10,15 +10,15 @@ b=1;
 ex = a + (b-a)*rand(size(coord,1),1);
 ey = a + (b-a)*rand(size(coord,1),1);
 % quando mas proximo a 1 eh mas ditorcido
-alpha=0.29;
-h=1/16;
+alpha=0.35;
+h=1000/25;
 
 for icoord=1:size(coord,1)
     x=coord(icoord,1);
     
     y=coord(icoord,2);
     if icoord>size(bedge,1) %&& abs(coord(icoord,1)-0.5)>1e-10
-       if single(y)~=0.5       
+      % if single(y)~=0.5       
 %        if x~=0.5 && y~=0.5
             x=coord(icoord,1)+h*alpha*ex(icoord,1);
             
@@ -27,11 +27,11 @@ for icoord=1:size(coord,1)
             auxcoord(icoord,1)=icoord;
             auxcoord(icoord,2)=x;
             auxcoord(icoord,3)=y;
-         else
-             auxcoord(icoord,1)=icoord;
-             auxcoord(icoord,2)=x;
-             auxcoord(icoord,3)=y;
-        end
+%          else
+%              auxcoord(icoord,1)=icoord;
+%              auxcoord(icoord,2)=x;
+%              auxcoord(icoord,3)=y;
+%         end
     else
 
         auxcoord(icoord,1)=icoord;
