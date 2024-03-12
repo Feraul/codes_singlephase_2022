d6cbe43f01e71318a2c2eec2a4398019d331b5e6
Fernando Contreras <ferlicapac@gmail.com>
2024-03-12 18:30:22 -0300

    ok

--- main.m
+++ main.m
@@ -57,7 +57,7 @@ erromethod='erromethod6';
 % nlfvHP    --> (NL-TPFA-H) metodo nao linear baseado em pontos harmonicos
 % nlfvPPS   --> 
 % interpfree
-pmetodo='lfvHP';
+pmetodo='mpfad';
 %% metodo de interacao: picard, newton, broyden, secant,
 % método de iterecao proprio de métodos nao lineares iterfreejacobian,iterdiscretnewton, JFNK
 % iteration='iterdiscretnewton';
