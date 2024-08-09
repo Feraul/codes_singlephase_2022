e66e0438b3e4a74619c54aa896ddf47d44fa2dc9
Fernando Contreras <ferlicapac@gmail.com>
2024-08-09 08:32:15 -0300

    ok

--- distortedramd.m
+++ distortedramd.m
@@ -10,8 +10,8 @@ b=1;
 ex = a + (b-a)*rand(size(coord,1),1);
 ey = a + (b-a)*rand(size(coord,1),1);
 % quando mas proximo a 1 eh mas ditorcido
-alpha=0.15;
-h=2;
+alpha=0.55;
+h=29;
 % h=0.25 --> 160x80
 %h=0.5 --> 80x40
 %h=0.95 -->40x20
@@ -19,29 +19,34 @@ h=2;
 
 for icoord=1:size(coord,1)
     x=coord(icoord,1);
-    
+
     y=coord(icoord,2);
     if icoord>size(bedge,1) %&& abs(coord(icoord,1)-0.5)>1e-10
-      % if single(y)~=0.5       
-%        if x~=0.5 && y~=0.5
+        % if single(y)~=0.5
+        %   if icoord~=433 & icoord~=434 & icoord~=459 & icoord~=460
+        %if icoord~=364 & icoord~=365 & icoord~=496 & icoord~=397 &...
+        %        icoord~=380 & icoord~=381 & icoord~=412 & icoord~=413 &...
+        %       icoord~=876 & icoord~=877 & icoord~=908 & icoord~=909 &...
+        %        icoord~=892 & icoord~=893 & icoord~=924 & icoord~=925
+         %  if (x~=0.5 && y~=0.5)
             x=coord(icoord,1)+h*alpha*ex(icoord,1);
-            
+
             y=coord(icoord,2)+h*alpha*ey(icoord,1);
-            
+
             auxcoord(icoord,1)=icoord;
             auxcoord(icoord,2)=x;
             auxcoord(icoord,3)=y;
-%          else
-%              auxcoord(icoord,1)=icoord;
-%              auxcoord(icoord,2)=x;
-%              auxcoord(icoord,3)=y;
+%         else
+%             auxcoord(icoord,1)=icoord;
+%             auxcoord(icoord,2)=x;
+%             auxcoord(icoord,3)=y;
 %         end
     else
 
         auxcoord(icoord,1)=icoord;
         auxcoord(icoord,2)=x;
         auxcoord(icoord,3)=y;
-        
+
     end
 end
 
